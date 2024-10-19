6c4a4ac015979861f36d6dd93dd4864c0badb751
jihanchen <jihanc@seas.upenn.edu>
2024-10-18 22:48:14 -0400

    Day 03, LC 707

---
+++ src/MyLinkedList.java
@@ -0,0 +1,47 @@
+class MyLinkedList {
+    /*
+    LeetCode 707:
+    Design your implementation of the linked list. You can choose to use a
+    singly or doubly linked list. A node in a singly linked list should have
+    two attributes: val and next. val is the value of the current node, and
+    next is a pointer/reference to the next node. If you want to use the doubly
+    linked list, you will need one more attribute prev to indicate the previous
+    node in the linked list. Assume all nodes in the linked list are 0-indexed.
+     */
+
+    // Initializes the MyLinkedList Object
+    public MyLinkedList() {
+
+    }
+
+    // Get the value of the index-th node in the linked list. If the index is
+    // invalid, return -1
+    public int get(int index) {
+        return -1;
+    }
+
+    // Add a node of value val before the first element of the linked list.
+    // After the insertion, the new node will be the first node of the linked
+    // list.
+    public void addAtHead(int val) {
+
+    }
+
+    // Append a node of value val as the last element of the linked list.
+    public void addAtTail(int val) {
+
+    }
+
+    // Add a node of value val before the index-th node in the linked list. If
+    // index equals the length of the linked list, the node will be appended to
+    // the end of the linked list. If index is greater than the length, the node
+    // will not be inserted.
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    // Deleted the index-th node in the linked list, if the index is valid.
+    public void deleteAtIndex(int index) {
+
+    }
+}
